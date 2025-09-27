e1a05df22c12d7d72235846fa738f007e1d16eef
Katelyn Wardell <kateward104@gmail.com>
2025-09-26 22:04:41 -0600

    added functions from the js assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { getParkData } from "./parkService.mjs";
 
-const parkData = getParkData();
+const data = getParkData();
+
+
 
 function parkInfoTemplate(info) {
     return `<a href="/" class="hero-banner__title:>${IntersectionObserver.name}</a>
@@ -10,13 +12,49 @@ function parkInfoTemplate(info) {
     </p>`;
 }
 
+function setHeaderInfo(data) {
+    // insert data into disclaimer section
+    const disclaimer = document.querySelector(".disclaimer > a");
+    disclaimer.href = data.url;
+    disclaimer.innerHTML = data.fullName;
+    //update the title of the site. Notice that we can select things in the head just like in the body with querySelector
+    document.querySelector("head > title").textContent = data.fullName;
+    // set the banner image
+    document.querySelector(".hero-banner > img").src = data.images[0].url;
+    // use the template function above to set the rest of the park specific info in the header
+    document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(data)
+}
 
-const disclaimer = document.querySelector(".disclaimer > a");
-disclaimer.href = parkData.url;
-disclaimer.innerHTML = parkData.fullName;
+function footer() {
+    /* output Mailing address and the voice phone number */
+}
 
-document.querySelector("head > title").textContent = parkData.fullName;
+mediaCardTemplate(info) {
+    // image, headline, and paragraph
+}
 
-document.querySelector(".hero-banner > img").src = parkData.images[0].url;
 
-document.querySelector(".hero-banner__content").innerHTML = parkInfoTemplate(parkData)
+setHeaderInfo(sampleParkData);
+
+
+const parkInfoLinks = [
+    {
+        name: "Current Conditions &#x203A;",
+        link: "conditions.html",
+        image: parkData.images[2].url,
+        description:
+            "See what conditions to expect in the park before leaving on your trip!"
+    },
+    {
+        name: "Fees and Passes &#x203A;",
+        link: "fees.html",
+        image: parkData.images[3].url,
+        description: "Learn about the fees and passes that are available."
+    },
+    {
+        name: "Visitor Centers &#x203A;",
+        link: "visitor_centers.html",
+        image: parkData.images[9].url,
+        description: "Learn about the visitor centers in the park."
+    }
+];
