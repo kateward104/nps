5c56133ed9a26ea3ffb215ef5f81ee2221807c08
Katelyn Wardell <kateward104@gmail.com>
2025-09-26 19:38:37 -0600

    finished adding javascript

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,21 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.title = parkData.fullName;
+
+const img = document.querySelector(".hero-banner img")
+img.src = parkData.images[0].url;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner__title:>${IntersectionObserver.name}</a>
+    <p class="hero-banner__subtitle">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+    </p>`;
+}
